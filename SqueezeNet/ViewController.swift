4246ccc630c10520858ebe5ce3dba45794163817
Jelena Tasic <jelenatasicsd@gmail.com>
2021-10-28 20:25:04 +0200

    Show selected image.

--- SqueezeNet/ViewController.swift
+++ SqueezeNet/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+    }
+    
+    //MARK:- UIImagePickerControllerDelegate Methods
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = pickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
-
+    @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
+        print("cameraButtonTapped")
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
